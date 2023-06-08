ffec2b239f31330ecec285788947f2c3b15a4c3c
Monkie <you@example.com>
2023-06-08 03:29:55 +0100

    d

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import SlotProps from '$lib/components/SlotProps/index.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<SlotProps />
